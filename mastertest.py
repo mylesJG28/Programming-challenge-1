1e814fe4fcbae84245aacdf0d96bcde08e26b8d7
Rajesh <myles.j.gould@ntlworld.com>
2014-03-27 06:28:14 -0400

    added tests

--- mastertest.py
+++ mastertest.py
@@ -5,3 +5,6 @@ class TestSequanceFunctions(unitest.TestCase):
 
     def test1(self):
         multiples.mults(5,20)==[5,10,15] #test 1 fails
+       
+        if __name__ == '__main__':
+    unittest.main()
